a5a82820f0702bee0ebe16f805149d41a4c7622f
rogody <jstonyone1@cau.ac.kr>
2026-04-30 21:59:09 +0900

    feat: 응답 통일 객체 생성

--- src/main/java/com/example/umc10th/global/apiPayload/ApiResponse.java
+++ src/main/java/com/example/umc10th/global/apiPayload/ApiResponse.java
@@ -1,4 +1,29 @@
 package com.example.umc10th.global.apiPayload;
 
-public class ApiResponse {
+import com.example.umc10th.global.apiPayload.code.BaseErrorCode;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"}) //json응답의 필드 순서 정렬용
+public class ApiResponse<T> {
+
+    @JsonProperty("isSuccess") //json 매핑에서 필드명을 지정
+    private final Boolean isSuccess;
+
+    @JsonProperty("code")
+    private final String code;
+
+    @JsonProperty("message")
+    private final String message;
+
+    @JsonProperty("result")
+    private T result;
+
+    public static <T> ApiResponse<T> onFailure(BaseErrorCode code, T result){
+        return new ApiResponse<>(false, code.getCode(), code.getMessage(), result);
+    }
 }
