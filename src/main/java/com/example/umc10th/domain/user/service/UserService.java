d869b08f204748cb7f15cfde6db35a2f305c9b26
rogody <jstonyone1@cau.ac.kr>
2026-05-06 00:58:21 +0900

    feat: 마이페이지 서비스 메서드 구현

--- src/main/java/com/example/umc10th/domain/user/service/UserService.java
+++ src/main/java/com/example/umc10th/domain/user/service/UserService.java
@@ -1,19 +1,34 @@
 package com.example.umc10th.domain.user.service;
 
+import com.example.umc10th.domain.user.controller.UserController;
+import com.example.umc10th.domain.user.converter.UserConverter;
 import com.example.umc10th.domain.user.dto.UserReqDTO;
 import com.example.umc10th.domain.user.dto.UserResDTO;
+import com.example.umc10th.domain.user.entity.Users;
+import com.example.umc10th.domain.user.exception.UserException;
+import com.example.umc10th.domain.user.exception.code.UserErrorCode;
+import com.example.umc10th.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    //마이페이지
     public UserResDTO.GetInfo getInfo(UserReqDTO.GetInfo dto) {
-        return UserResDTO.GetInfo.builder()
-                .name("유저 예시 1")
-                .profileUrl("https://example.com/profile/1.png")
-                .email("test@example.com")
-                .phoneNumber("010-1234-5678")
-                .point(1500)
-                .build();
+
+        //dto에서 유저 id 추출
+        Long userId = dto.id();
+        // db에서 해당 유저 ID로 데이터 조회
+        Users user = userRepository.findById(userId)
+                .orElseThrow(()-> new UserException(UserErrorCode.USER_NOT_FOUND));
+
+        // 컨버터를 이용해 응답 DTO 생성 & return
+        return UserConverter.toGetInfo(user);
     }
 
     //회원 가입 메서드
