1a6bdfef1bbad9a3f65352b8e6ff48883f0a6ff7
rogody <jstonyone1@cau.ac.kr>
2026-05-05 23:06:02 +0900

    리뷰 도메인 엔티티 제작 및 양뱡향 매핑

--- src/main/java/com/example/umc10th/domain/review/entity/ReviewPhoto.java
+++ src/main/java/com/example/umc10th/domain/review/entity/ReviewPhoto.java
@@ -1,4 +1,27 @@
 package com.example.umc10th.domain.review.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "review_photo")
 public class ReviewPhoto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "photo_url", nullable = false)
+    private String photoUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
 }
